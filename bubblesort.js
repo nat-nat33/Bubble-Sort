efb65224e2d8662b3c966e8c441f5861e1da4695
Juan Jose Gonzalez Macias <JuanJo@MacBook-Pro-de-Juan-Jose-Gonzalez-Macias.local>
2016-03-29 15:43:02 -1000

    adding additional tests for prototype

--- bubblesort.js
+++ bubblesort.js
@@ -12,4 +12,22 @@ function myBubbleSort(arr) {
   return arr;
 }
 
+Array.prototype.myBubbleSort = function () {
+  var length = this.length;
+  for (var i = 0; i < length; i++) {
+    for (var j = 0; j < (length - i - 1); j++) {
+      if(this[j] > this[j+1]) {
+        var tmp = this[j];
+        this[j] = this[j+1];
+        this[j+1] = tmp;
+      }
+    }
+  }
+  return this;
+};
+
+
+
+
+
 module.exports = myBubbleSort;
--- test/bubblesort.spec.js
+++ test/bubblesort.spec.js
@@ -9,6 +9,10 @@ describe('My Bubble Sort', function(){
     expect(myBubbleSort).to.be.a('function');
   });
 
+  it('should take in an array argument', function () {
+    expect(arguments).to.equal(arguments);
+  });
+
   it('should return sorted array', function(){
     var myArr2 = [4,3,2,1];
     var myArr3 = [5,1,4,7];
@@ -21,3 +25,21 @@ describe('My Bubble Sort', function(){
 
 });
 
+describe('Prototype My Bubble Sort', function () {
+  var myArr2 = [4,3,2,1];
+  var myArr3 = [5,1,4,7];
+  var myArr4 = [10,9,21,35,17];
+
+  it('should be a method on array prototype', function () {
+    myArr2.should.have.property('myBubbleSort');
+    myArr3.should.have.property('myBubbleSort');
+    myArr4.should.have.property('myBubbleSort');
+  });
+
+  it('should return a sorted array', function () {
+    expect(myArr2.myBubbleSort()).to.deep.equal([1,2,3,4]);
+    expect(myArr3.myBubbleSort()).to.deep.equal([1,4,5,7]);
+    expect(myArr4.myBubbleSort()).to.deep.equal([9,10,17,21,35]);
+  });
+
+});
