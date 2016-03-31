94a1f2b2a71eeb09a4d8932125dfa4e2429e6560
Juan Jose Gonzalez Macias <JuanJo@MacBook-Pro-de-Juan-Jose-Gonzalez-Macias.local>
2016-03-30 15:28:12 -1000

    adding quicksort and quicksort testing

---
+++ public/js/quicksort.js
@@ -0,0 +1,21 @@
+function quickSort (arr) {
+  if(arr.length === 0){
+    return [];
+  }
+
+  var left = [];
+  var right = [];
+  var pivot = arr[0];
+
+  for(var i = 1; i < arr.length; i ++){
+    if(arr[i] < pivot){
+      left.push(arr[i]);
+      console.log(left);
+    }
+      else {
+        right.push(arr[i]);
+        console.log(right);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
+}
